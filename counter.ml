d283c6f2a39cb4654e532bf6420e6500d21e2ac4
Frédéric LOYER <frederic.loyer@club-internet.fr>
2023-04-28 11:21:05 +0200

    Minor improvements

--- counter.ml
+++ counter.ml
@@ -7,7 +7,7 @@ let hbox = GPack.hbox ~border_width:10 ~packing:w#add ()
 let label = GMisc.label ~packing:(hbox#pack ~padding:4) ~text:"0" ()
 let button = GButton.button ~label:("Count") ~packing:(hbox#pack ~padding:4) ()
 
-let _ = button#connect#clicked ~callback:(function _ ->
+let _ = button#connect#clicked ~callback:(function () ->
   incr counter;
   label#set_text (Int.to_string !counter))
 
